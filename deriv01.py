57b0258a053cbd21300617cfc4de1791273b631d
Zadok <119855051+Zadok-01@users.noreply.github.com>
2023-01-23 08:22:28 +0000

    Add files via upload

---
+++ deriv01.py
@@ -0,0 +1,20 @@
+
+from math import sin, cos, pi
+
+def derivative(func, x, h):
+	return (func(x + h) - func(x)) / h
+
+x = pi / 4
+diffs = {}
+
+for j in range(21):
+	h = 10 ** -j
+	deriv = derivative(sin, x, h)
+	exact = cos(x)
+	diff = abs(deriv - exact)
+	diffs[j] = diff
+
+print(diffs)
+for k, v in diffs.items():
+	print(f'{k}:\t{v}')
+
---
+++ diffcomplex.py
@@ -0,0 +1,21 @@
+
+h = 1e-6
+
+def square(x):
+	return x * x
+
+def var(x):
+	return complex(x, h)
+
+def diff_cx(f, x):
+	return f(x).imag / h
+
+print('func\tx\tz\t\tfunc(x)\t\tdiff(func, z)')
+
+f = square
+x = 4
+z = var(x)
+d = diff_cx
+
+print(f'{f.__name__}\t{x}\t{z}\t{f(x)}\t\t{d(f, z)}')
+
