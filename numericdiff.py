c78139974ab91fea8f0e56934be5bcc4763f8ed3
Zadok <119855051+Zadok-01@users.noreply.github.com>
2023-01-23 08:26:44 +0000

    Add files via upload

---
+++ numericdiff.py
@@ -0,0 +1,30 @@
+DELTA_X = h = 1e-6
+
+def square(x):
+    return x * x
+
+def cube(x):
+    return x * x * x
+
+def diff_f(f, x):
+    return (f(x + h) - f(x)) / h
+
+def diff_b(f, x):
+    return (f(x) - f(x - h)) / h
+
+def diff_c(f, x):
+    return (f(x + h) - f(x - h)) / 2 / h
+
+def stencil_5(f, x):
+    return (-f(x + 2 * h) + 8 * f(x + h) - 8 * f(x - h) + f(x - 2 * h)) / 12 / h
+
+gradients =[diff_f, diff_b, diff_c, stencil_5]
+funcs = [square, cube]
+x_values = [4, 3]
+
+print('gradient\tfunction\tx_value\tf(x)\tgradient')
+for f, x in zip(funcs, x_values):
+    print()
+    for g in gradients:
+        print(f'{g.__name__}\t{f.__name__}\t{x}\t{f(x)}\t{g(f, x)}')
+
---
+++ numericinteg.py
@@ -0,0 +1,66 @@
+# Numeric Integration
+
+from math import exp
+
+def integ1(f, a, b, n=100):
+    # Trapezoidal
+    w = (b - a) / n
+    seq = (a + i * w for i in range(n + 1))
+    s = 2 * sum(map(f, seq))
+    s = s - f(a) - f(b)
+    return s * w / 2
+
+def integ2(f, a, b, n=100):
+    # Mid-point
+    w = (b - a) / n
+    seq = (a + (i + .5) * w for i in range(n))
+    s = sum(map(f, seq))
+    return s * w
+
+def integ3(f, a, b, n=100):
+    # Simpson's Rule (Thirds) or (1+4+1 / 6)
+    w = (b - a) / n
+    s = 0
+    for i in range(n):
+        x1 = a + i * w
+        x3 = x1 + w
+        x2 = (x1 + x3) / 2
+        s += f(x1) + 4 * f(x2) + f(x3)
+    return s * w / 6
+
+def integ4(f, a, b, n=100):
+    # Simpson's Second Rule (Three Eighths) or (1+3+3+1 / 8)
+    w = (b - a) / n
+    s = 0
+    for i in range(n):
+        x1 = a + i * w
+        x4 = x1 + w
+        x2 = (2 * x1 + x4) / 3
+        x3 = (x1 + 2 * x4) / 3
+        s += f(x1) + 3 * f(x2) + 3 * f(x3) + f(x4)
+    return s * w / 8
+
+def square(x):
+    return x * x
+
+def func2(t):
+    return 3 * t ** 2 * exp(t ** 3)
+
+a = 0      #3  # lower limit
+b = 1      #5  # upper limit
+f = func2      #square
+
+for n in range(1, 201):
+    print('{:>4}   {:<22.18}   {:<22.18}   {:<22.18}   {:<22.18}'.format(
+            n, 
+            integ1(f, a, b, n), 
+            integ2(f, a, b, n), 
+            integ3(f, a, b, n), 
+            integ4(f, a, b, n)))
+
+print('--------')
+print('       {:<22.18}   {:<22.18}   {:<22.18}   {:<22.18}'.format(
+        integ1(f, a, b), 
+        integ2(f, a, b), 
+        integ3(f, a, b), 
+        integ4(f, a, b)))
